0e393c17bbb220f960b31d83cc0d07b67e792920
Tommaso Negri <tommasongr@icloud.com>
2021-06-08 22:31:41 +0200

    fix: Correct scrollInto element target

--- pages/narratives/data-availability.vue
+++ pages/narratives/data-availability.vue
@@ -3,14 +3,12 @@
     <narrative-header
       :chapter-number="1"
       title="Why is important to make data available in the Arab Region?"
+      scroll-into-element-id="scroll-into-target"
     >
       <template #background>
         <div style="background: black; height: 100%; width: 100%"></div>
       </template>
     </narrative-header>
-    <h1 id="narrative-content">
-      Why is important to make data available in the Arab Region?
-    </h1>
   </div>
 </template>
 
