213796d76a349c58444ef76455ab1aea870ffa1e
Tommaso Negri <tommasongr@icloud.com>
2021-06-26 00:07:18 +0200

    (:WIP) feat: Improve chart, table, animations and yAxis

--- components/narrative/data-disaggregation/DataDisaggregationViz.vue
+++ components/narrative/data-disaggregation/DataDisaggregationViz.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="viz-container">
-    <svg id="data-disaggregation-viz" width="100%" height="100%"></svg>
-  </div>
+  <div id="data-disaggregation-viz" class="viz-container"></div>
 </template>
 
 <script>
@@ -10,13 +8,26 @@ import developmentGoals from '~/data/development-goals.json'
 export default {
   name: 'DataDisaggregationVizComponent',
 
+  props: {
+    disaggregation: {
+      type: String,
+      required: true,
+    },
+  },
+
   data() {
     return {
       developmentGoals: null,
       vizData: null,
       goalsData: null,
-      disaggregation: 'Sex',
+      maxIndicatorCodes: 0,
     }
+  },
+
+  watch: {
+    disaggregation(oldValue, newValue) {
+      this.updateViz(oldValue, newValue)
+    },
   },
 
   async mounted() {
@@ -31,8 +42,28 @@ export default {
     const responseGoalsDataRawText = await responseGoalsData.text()
 
     this.vizData = this.$d3.csvParse(responseVizDataRawText)
+
+    const parsedDisaggregations = []
     this.vizData.forEach((d) => {
       d.goal_code = +d.goal_code
+
+      // Looping through dataset to find the maximum number of Indicator Codes.
+      // This is used to proportionally display the Indicators on the xAxis
+      if (!parsedDisaggregations.includes(d.disaggregation)) {
+        const codesSet = new Set()
+
+        this.vizData
+          .filter((entry) => entry.disaggregation === d.disaggregation)
+          .forEach((entry) => {
+            codesSet.add(entry.indicator_code)
+          })
+
+        if (codesSet.size > this.maxIndicatorCodes) {
+          this.maxIndicatorCodes = codesSet.size
+        }
+
+        parsedDisaggregations.push(d.disaggregation)
+      }
     })
     this.vizData.sort(function (a, b) {
       return a.goal_code - b.goal_code
@@ -48,23 +79,17 @@ export default {
 
   methods: {
     drawViz(vizData, goalsData) {
-      const svg = this.$d3.select('#data-disaggregation-viz')
+      const container = this.$d3.select('#data-disaggregation-viz')
 
       const containerWidth = 100
       const containerHeight = 100
 
-      // const safeAreaMargins = {
-      //   top: 40,
-      //   right: 0,
-      //   bottom: 0,
-      //   left: 120,
-      // }
-
-      const rectsGroup = svg
-        .append('g')
-        .style('position', 'relative')
-        .attr('width', containerHeight + '%')
-        .attr('height', containerHeight + '%')
+      const safeAreaMargins = {
+        top: 40,
+        right: 0,
+        bottom: 0,
+        left: 200,
+      }
 
       /* ----------- SCALES ----------- */
 
@@ -86,23 +111,95 @@ export default {
         .domain(vizData.map(yValue).sort())
         .range([0, containerHeight])
         .paddingInner(0.3)
+        .paddingOuter(0.15)
+        .align(0.5)
 
-      // console.log(xScale.domain())
-      // console.log(xScale.range())
-      // console.log(yScale.domain())
-      // console.log(yScale.range())
+      /* ----------- TABLE ROWS (Alternating colors) ----------- */
+
+      const tableRowsGroup = container
+        .append('div')
+        .style('position', 'absolute')
+        .style('width', '100%')
+        .style(
+          'height',
+          `calc(100% - ${safeAreaMargins.top}px - ${safeAreaMargins.bottom}px)`
+        )
+        .style('bottom', 0)
+
+      yScale.domain().forEach((d, index) => {
+        tableRowsGroup
+          .append('div')
+          .style('position', 'absolute')
+          .style('top', () => {
+            return yScale.step() * index + '%'
+          })
+          .style('width', '100%')
+          .style('height', `calc(100% / ${yScale.domain().length})`)
+          .style('background-color', () => {
+            if (index % 2 === 0) {
+              return 'none'
+            } else {
+              return '#0F1D23'
+            }
+          })
+      })
+
+      /* ----------- AXIS ----------- */
+
+      const yAxisGroup = container
+        .append('div')
+        .classed('y-axis-container', true)
+        .style(
+          'height',
+          `calc(100% - ${safeAreaMargins.top}px - ${safeAreaMargins.bottom}px)`
+        )
+        .style('bottom', 0)
+
+      yAxisGroup
+        .selectAll('span')
+        .data(yScale.domain())
+        .enter()
+        .append('div')
+        .style('height', `calc(100% / ${yScale.domain().length})`)
+        .append('span')
+        .text((d) => d)
 
       /* ----------- RECTS ----------- */
 
+      const rectsGroupSafeArea = container
+        .append('div')
+        .style('position', 'absolute')
+        .style('z-index', 10)
+        .style(
+          'width',
+          `calc(100% - ${safeAreaMargins.right}px - ${safeAreaMargins.left}px)`
+        )
+        .style(
+          'height',
+          `calc(100% - ${safeAreaMargins.top}px - ${safeAreaMargins.bottom}px)`
+        )
+        .style('right', 0)
+        .style('bottom', 0)
+
+      const rectsGroup = rectsGroupSafeArea
+        .append('div')
+        .style('position', 'absolute')
+        .style(
+          'width',
+          (xScale.domain().length * 100) / this.maxIndicatorCodes + '%'
+        )
+        .style('height', '100%')
+
       rectsGroup
-        .selectAll('rect')
+        .selectAll('div')
         .data(vizData.filter((d) => d.disaggregation === this.disaggregation))
         .enter()
-        .append('rect')
-        .attr('x', (d) => xScale(xValue(d)) + '%')
-        .attr('y', (d) => yScale(yValue(d)) + '%')
-        .attr('width', xScale.bandwidth() + '%')
-        .attr('height', yScale.bandwidth() + '%')
+        .append('div')
+        .style('position', 'absolute')
+        .style('left', (d) => xScale(xValue(d)) + '%')
+        .style('top', (d) => yScale(yValue(d)) + '%')
+        .style('width', xScale.bandwidth() + '%')
+        .style('height', yScale.bandwidth() + '%')
         .attr('class', (d) => {
           if (d.availability === '0.5') {
             return 'data-disaggregation-viz-rect data-disaggregation-viz-rect--masked'
@@ -110,7 +207,7 @@ export default {
             return 'data-disaggregation-viz-rect'
           }
         })
-        .attr('fill', (d) => {
+        .style('background-color', (d) => {
           if (d.availability === '0') {
             return 'none'
           } else {
@@ -118,6 +215,32 @@ export default {
               .color
           }
         })
+        .style('opacity', 0)
+
+      const t = this.$d3.transition().duration(800).ease(this.$d3.easeQuadOut)
+
+      this.$d3
+        .selectAll('.data-disaggregation-viz-rect')
+        .transition(t)
+        .delay(() => Math.floor(Math.random() * (500 - 0 + 1) + 0))
+        .style('opacity', 1)
+    },
+
+    updateViz(oldValue, newValue) {
+      const t = this.$d3.transition().duration(500).ease(this.$d3.easeQuadOut)
+
+      this.$d3
+        .selectAll('.data-disaggregation-viz-rect')
+        .transition(t)
+        .delay(() => Math.floor(Math.random() * (300 - 0 + 1) + 0))
+        .style('opacity', 0)
+
+      setTimeout(() => {
+        const container = this.$d3.select('#data-disaggregation-viz')
+        container.selectChildren('*').remove()
+
+        this.drawViz(this.vizData, this.goalsData)
+      }, 800)
     },
   },
 }
@@ -126,6 +249,7 @@ export default {
 <style lang="scss" scoped>
 .viz-container {
   height: 100%;
+  width: 100%;
   position: relative;
 
   & svg {
@@ -136,14 +260,32 @@ export default {
 
 <style lang="scss">
 .data-disaggregation-viz-rect {
-  stroke: rgba(255, 255, 255, 0.2);
-  stroke-width: 2px;
+  border: solid 1px rgb(64, 64, 64);
 }
 
 .data-disaggregation-viz-rect--masked {
-  mask-image: url(~/assets/images/narrative/data-disaggregation/cuts-mask.svg);
-  mask-repeat: no-repeat;
-  mask-size: cover;
-  mask-type: luminance;
+  background-image: url(~/assets/images/narrative/data-disaggregation/cuts-mask.svg);
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.y-axis-container {
+  position: absolute;
+  left: 0;
+  width: 180px;
+  height: 100%;
+
+  & div {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    & span {
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      direction: rtl;
+    }
+  }
 }
 </style>
