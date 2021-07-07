d9a593e541ab5b57c58a0ba37baa05ab3a483675
uf0 <scnepa222@gmail.com>
2021-07-07 11:42:35 +0200

    wip data up to date

--- plugins/d3.js
+++ plugins/d3.js
@@ -2,12 +2,27 @@ import Vue from 'vue'
 
 import { csvParse } from 'd3-dsv'
 import { select, selectAll } from 'd3-selection'
-import { scaleBand, scaleLinear, scaleOrdinal, scaleSqrt } from 'd3-scale'
-import { axisLeft } from 'd3-axis'
+import {
+  scaleBand,
+  scaleLinear,
+  scaleOrdinal,
+  scaleSqrt,
+  scaleTime,
+} from 'd3-scale'
+import { axisLeft, axisTop } from 'd3-axis'
 import { transition } from 'd3-transition'
 import { easeQuadOut } from 'd3-ease'
-import { rollups, median, descending, range } from 'd3-array'
-import { arc } from 'd3-shape'
+import {
+  rollups,
+  median,
+  descending,
+  range,
+  extent,
+  merge,
+  sum,
+  max,
+} from 'd3-array'
+import { arc, line } from 'd3-shape'
 
 const d3 = {
   // DSV
@@ -22,9 +37,11 @@ const d3 = {
   scaleLinear,
   scaleOrdinal,
   scaleSqrt,
+  scaleTime,
 
   // AXIS
   axisLeft,
+  axisTop,
 
   // TRANSITION
   transition,
@@ -37,9 +54,14 @@ const d3 = {
   median,
   descending,
   range,
+  extent,
+  merge,
+  sum,
+  max,
 
   // SHAPE
   arc,
+  line,
 }
 
 Object.defineProperty(Vue.prototype, '$d3', { value: d3 })
