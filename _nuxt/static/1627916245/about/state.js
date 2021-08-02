b87c0ceea5428abe61df87c26fe925d0eafb6417
uf0 <uf0@users.noreply.github.com>
2021-08-02 14:57:33 +0000

    deploy: 0676250075426018eeffc5add3c865db92731a42

---
+++ _nuxt/static/1627916245/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fundp-data-gaps\u002F_nuxt\u002Fstatic\u002F1627916245",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Fundp-data-gaps\u002F",assetsPath:"\u002Fundp-data-gaps\u002F_nuxt\u002F",cdnURL:a}}}}(null));
